ee1d8e4e834758313dd0c1634020e66bc724ac90
Yehudit-Leibowitz <38214938334@mby.co.il>
2025-02-27 14:38:57 +0200

    slider

--- src/app/components/basic-components/yv-slider/yv-slider.component.ts
+++ src/app/components/basic-components/yv-slider/yv-slider.component.ts
@@ -1,6 +1,6 @@
 
 
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-yv-slider',
@@ -9,11 +9,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './yv-slider.component.html',
   styleUrl: './yv-slider.component.scss'
 })
-export class YvSliderComponent {
+export class YvSliderComponent implements OnInit{
+  colorWidth? : string ;
+  
+  ngOnInit(): void {
+ this.colorWidth =  `${(this.value / 100) * 8.5}rem`;
+    
+  }
   @Input() value: number = 50;
 
-  getColoredWidth(): string {
-    return `${(this.value / 100) * 8.5}rem`;
-  }
 
 }
