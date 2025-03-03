a3f31a5003b2cdc29dd2dc227734c428e37ace23
efrat2684 <38215739889@mby.co.il>
2025-03-03 10:43:39 +0200

    change the class names & the colors

---
+++ src/app/components/basic-components/toast-notification/toast-notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yv-cluster-toast-notification',
+  standalone: true,
+  imports: [],
+  templateUrl: './toast-notification.component.html',
+  styleUrl: './toast-notification.component.scss'
+})
+export class ToastNotificationComponent {
+
+}
--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { ButtonType } from 'src/app/enums/basic-enum';
 import { CommonModule } from '@angular/common';
 import { TempButtonComponent } from '../basic-components/temp-button/temp-button.component';
+import { BasicTabComponent } from '../basic-components/basic-tab/basic-tab.component';
+import { BasicCardComponent } from '../basic-components/basic-card/basic-card.component';
+import { BasicRadioButtonComponent } from '../basic-components/basic-radio-button/basic-radio-button.component';
+import { BasicToggleComponent } from '../basic-components/basic-toggle/basic-toggle.component';
+import { FilterSectionComponent } from '../basic-components/filter-section/filter-section.component';
+import { ToastNotificationComponent } from '../basic-components/toast-notification/toast-notification.component';
+import { SwitchComponent } from '../basic-components/switch/switch.component';
+import { CardIcons } from 'src/app/enums/card-icons-enum';
 
 @Component({
   selector: 'yv-cluster-test',
   standalone: true,
-  imports: [CommonModule, TempButtonComponent],
+  imports: [CommonModule, TempButtonComponent,BasicTabComponent,BasicCardComponent,BasicRadioButtonComponent,BasicToggleComponent,FilterSectionComponent,ToastNotificationComponent,SwitchComponent],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
@@ -16,13 +24,31 @@ export class TestComponent {
   btnType2 = ButtonType.SECONDARY;
   txt1 = "test btn1"
   // txt1 = "test btn1"
-
-
-
+  switchState: boolean = false;
+  tabState: boolean = false;
+  toggleState: boolean = false;
+  radioButtonState: boolean = false;
+  CardIcons = CardIcons;
   onClick()
   {
     alert('test on click');
     console.log('test on click');
   }
+  handleSwitchChange(state: boolean) {
+    this.switchState = state;
+    console.log('מצב ה-Switch:', state ? 'דלוק' : 'מכובה');
+  }
 
+  handleTabChange(state: boolean) {
+    this.tabState = state;
+    console.log('מצב ה-Tab:', state ? 'דלוק' : 'מכובה');
+  }
+  handleToggleChange(state: boolean) {
+    this.toggleState = state;
+    console.log('מצב ה-toggle:', state ? 'דלוק' : 'מכובה');
+  }
+  handleRadioButtonChange(state: boolean) {
+    this.radioButtonState = state;
+    console.log('מצב ה-radioButton:', state ? 'דלוק' : 'מכובה');
+  }
 }
