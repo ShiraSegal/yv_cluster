d0fc05e4f510c7e2f70bfac87bc6522c1abd7f23
rivky22337 <38214919813@mby.co.il>
2025-03-05 19:07:03 +0200

    strat_cmpare_modal_button

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../basic-components/button/button.component';
-import { ButtonIconProperty, ButtonType } from '../../enums/basic-enum';
+import { ButtonIconProperty, ButtonType,SquareIconButtonIcon } from '../../enums/basic-enum';
 import { CommonModule } from '@angular/common';
+import { BasicSquareIconButtonComponent } from '../basic-components/basic-square-icon-button/basic-square-icon-button.component';
+import { CompareModalButtonComponent } from '../basic-components/compare-modal-button/compare-modal-button.component';
 
 @Component({
   selector: 'app-test',
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent,BasicSquareIconButtonComponent,CompareModalButtonComponent],
   standalone: true,
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css'],
@@ -19,6 +21,9 @@ export class TestComponent {
   variant1=ButtonIconProperty.VARIANT1
   variant2=ButtonIconProperty.VARIANT2
   variant3=ButtonIconProperty.VARIANT3
+  list=SquareIconButtonIcon.LIST
+  image=SquareIconButtonIcon.IMAGE
+
   onClick()
   {
     alert('test on click');
--- src/app/enums/basic-enum.ts
+++ src/app/enums/basic-enum.ts
@@ -8,3 +8,12 @@ VARIANT1="variant1",
 VARIANT2="variant2",
 VARIANT3="variant3"
 }
+export enum SquareIconButtonIcon{
+  LIST="fa-solid fa-list",
+  IMAGE="fa-solid fa-image"
+}
+export enum SmallIconButtonIcon{
+  TRASH="fa-solid fa-trash",
+  LIST="fa-solid fa-clipboard-list"
+}
+
