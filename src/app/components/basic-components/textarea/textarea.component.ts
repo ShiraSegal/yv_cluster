f71e6f1dfcef5cf09f9f8818d731455f58562a01
רחל חיה ליף <327796876@mbydomain>
2025-03-09 13:06:00 +0200

    merge with main after finish table

--- src/app/components/basic-components/textarea/textarea.component.ts
+++ src/app/components/basic-components/textarea/textarea.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'yv-cluster-textarea',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './textarea.component.html',
   styleUrl: './textarea.component.scss'
 })
 export class TextareaComponent {
-
+  @Input() label: string = '';
+  @Input() textControl:FormControl = new FormControl<string>('');
+  isActive = false;
+ 
+  onFocus() {
+    this.isActive = true;
+  }
+ 
+  onBlur() {
+    if (this.textControl.value === '') {
+      this.isActive = false;
+    }
+  }
 }
