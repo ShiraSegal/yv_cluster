70278ed5e4e771ae9922e31dc4e7defe12285a9e
38214938334@mby.co.il <38214938334@mby.co.il>
2025-03-09 14:47:05 +0200

    final checkbox

--- src/app/components/basic-components/check/check.component.ts
+++ src/app/components/basic-components/check/check.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'yv-cluster-check',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './check.component.html',
   styleUrl: './check.component.scss'
 })
 export class CheckComponent {
+  @Input() type: string = 'unselected';
+  @Input() state: string = 'enabled';
 
+
+  toggleCheckbox() {
+    if (this.state !== 'desabled') {
+      this.type = this.type === 'selected' ? 'unselected' : 'selected';
+    }
+  }
 }
