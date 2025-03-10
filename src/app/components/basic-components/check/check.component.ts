3c7daa1013c277349793c558469c598aa1e186db
Yehudit-Leibowitz <38214938334@mby.co.il>
2025-03-10 17:23:32 +0200

    check with the option of 'checked'

--- src/app/components/basic-components/check/check.component.ts
+++ src/app/components/basic-components/check/check.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { CheckStateType, CheckType } from 'src/app/enums/basic-enum';
 
 @Component({
   selector: 'yv-cluster-check',
@@ -9,13 +10,22 @@ import { Component, Input } from '@angular/core';
   styleUrl: './check.component.scss'
 })
 export class CheckComponent {
-  @Input() type: string = 'unselected';
-  @Input() state: string = 'enabled';
+  @Input() type: CheckType =CheckType.UNCHECKED ;
+  @Input() state: CheckStateType = CheckStateType.ENABLED;
+  // @Input() type: CheckType =CheckType.UNSELECTED ;
+  // @Input() state: CheckStateType = CheckStateType.ENABLED;
 
-
+  CheckType=CheckType;
+  CheckStateType=CheckStateType;
+  
+  // toggleCheckbox() {
+  //   if (this.state !== CheckStateType.DISABLED) {
+  //     this.type = this.type === CheckType.SELECTED ?CheckType.UNSELECTED : CheckType.SELECTED;
+  //   }
+  // }
   toggleCheckbox() {
-    if (this.state !== 'desabled') {
-      this.type = this.type === 'selected' ? 'unselected' : 'selected';
+    if (this.state !== CheckStateType.DISABLED) {
+      this.type = this.type === CheckType.CHECKED ?CheckType.UNCHECKED : CheckType.CHECKED;
     }
   }
 }
