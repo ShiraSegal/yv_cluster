51137b33e786e1aaee74b68797f6951bc1deaafe
Yehudit-Leibowitz <38214938334@mby.co.il>
2025-03-10 17:32:32 +0200

    checkbox after code reviwe

--- src/app/components/basic-components/check/check.component.ts
+++ src/app/components/basic-components/check/check.component.ts
@@ -12,17 +12,11 @@ import { CheckStateType, CheckType } from 'src/app/enums/basic-enum';
 export class CheckComponent {
   @Input() type: CheckType =CheckType.UNCHECKED ;
   @Input() state: CheckStateType = CheckStateType.ENABLED;
-  // @Input() type: CheckType =CheckType.UNSELECTED ;
-  // @Input() state: CheckStateType = CheckStateType.ENABLED;
+
 
   CheckType=CheckType;
   CheckStateType=CheckStateType;
-  
-  // toggleCheckbox() {
-  //   if (this.state !== CheckStateType.DISABLED) {
-  //     this.type = this.type === CheckType.SELECTED ?CheckType.UNSELECTED : CheckType.SELECTED;
-  //   }
-  // }
+
   toggleCheckbox() {
     if (this.state !== CheckStateType.DISABLED) {
       this.type = this.type === CheckType.CHECKED ?CheckType.UNCHECKED : CheckType.CHECKED;
