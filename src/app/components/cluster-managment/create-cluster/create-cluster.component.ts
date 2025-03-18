cc03b6eefb241c02552a7398661e4092a0dc36e8
ShaniShulman <38327840369@mby.co.il>
2025-03-19 01:12:51 +0200

    continue form create cluster

--- src/app/components/cluster-managment/create-cluster/create-cluster.component.ts
+++ src/app/components/cluster-managment/create-cluster/create-cluster.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonComponent } from '../../basic-components/button/button.component';
 import { HeadingComponent } from '../../basic-components/heading/heading.component';
 import { BasicRadioButtonComponent } from '../../basic-components/basic-radio-button/basic-radio-button.component';
@@ -9,19 +9,21 @@ import { BodyComponent } from '../../basic-components/body/body.component';
 import { TableHeaderComponent } from '../../basic-components/table-header/table-header.component';
 import { YvSelectComponent } from '../../basic-components/yv-select/yv-select.component';
 import { RadioButtonListComponent } from '../../basic-components/radio-button-list/radio-button-list.component';
+import { TextareaComponent } from '../../basic-components/textarea/textarea.component';
+import { ClusterApiService } from 'src/app/services/cluster-api.service';
 
 @Component({
   selector: 'yv-cluster-create-cluster',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule,CommonModule, ButtonComponent, HeadingComponent, BasicRadioButtonComponent,RadioButtonListComponent, BodyComponent, TableHeaderComponent, YvSelectComponent, ButtonComponent],
+  imports: [FormsModule, ReactiveFormsModule,CommonModule, ButtonComponent, HeadingComponent, BasicRadioButtonComponent,RadioButtonListComponent, BodyComponent, TableHeaderComponent, YvSelectComponent, ButtonComponent,TextareaComponent],
   templateUrl: './create-cluster.component.html',
   styleUrl: './create-cluster.component.scss'
 })
 export class CreateClusterComponent {
+  constructor(private clusterData: ClusterApiService) { }
   formGroup: FormGroup = new FormGroup({});
   header: string = 'Create Cluster';
   size: TextSize = TextSize.SMALL;
-  // selectedLabel: string = 'Cluster Name';
   weight: TextWeight = TextWeight.BOLD;
   color: TextColor = TextColor.NEUTRAL_GRAY;
   tableHeader1: string = 'Field';
@@ -29,7 +31,7 @@ export class CreateClusterComponent {
   weight1: TextWeight = TextWeight.BOLD;
   tableHeader2: string = 'Text Value';
   color1: TextColor = TextColor.SLATE_BLUE;
-  dataCells: string[] = ['First Name', 'Last Name', 'Maiden Name', 'Place Of Birth', 'Authentic Date Of Birth', 'Restored Date Of Birth', 'Permanent Place', 'Place of Death', 'Gender', 'Fate'];
+  dataCells: string[] = [];//['First Name', 'Last Name', 'Maiden Name', 'Place Of Birth', 'Authentic Date Of Birth', 'Restored Date Of Birth', 'Permanent Place', 'Place of Death', 'Gender', 'Fate'];
   hederType: HeaderCellType = HeaderCellType.TEXT;
   options:string[]=['first name','שם בעברית','other'];
   // label1: string = 'first name';
@@ -44,8 +46,18 @@ export class CreateClusterComponent {
   btn_size2: ButtonSize = ButtonSize.SMALL;
   buttomType1: ButtonType = ButtonType.TERTIARY;
   buttomType2: ButtonType = ButtonType.PRIMARY;
+  radioControl = new FormControl<string | null>(null);
 
-  selectionChange(){
+  ngOnInit() {
+  this.clusterData.getCreateClusterData().subscribe((data) => {
+    this.dataCells.push(data[0]);
+    console.log("ddddddddddddddddddd",data);
     
+  });
+   this.radioControl.setValue(this.options[0]);
+  }
+  onSelectionChanged(selectedOption: string) {
+    this.radioControl.setValue(selectedOption);
+
   }
 }
--- src/app/services/cluster-api.service.ts
+++ src/app/services/cluster-api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -8,11 +9,14 @@ import { Injectable, inject } from '@angular/core';
 export class ClusterApiService {
   basicParam: string = 'reservations';
   #http = inject(HttpClient);
-
+  apiUrl = 'assets/json-data';
   async getAutoClusterData() {
     return this.#http.get<string[]>('/assets/getAutoCluster.json');
   }
 
+   getCreateClusterData():Observable<string[]> {
+    return this.#http.get<string[]>(`${this.apiUrl}/getCreateClusterData.json`);
+  }
   //environment.apiUrl + this.basicParam + '/' + reservationNumber +"?lang="+lang+"&ts="+new Date().valueOf()
 
 }
