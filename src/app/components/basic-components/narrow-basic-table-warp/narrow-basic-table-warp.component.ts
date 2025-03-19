fce5e7fdfe30853b9ffdc07769b2294ec6bf5a68
Yehudit-Leibowitz <38214938334@mby.co.il>
2025-03-19 19:04:49 +0200

    middle work on the narrow -warp

--- src/app/components/basic-components/narrow-basic-table-warp/narrow-basic-table-warp.component.ts
+++ src/app/components/basic-components/narrow-basic-table-warp/narrow-basic-table-warp.component.ts
@@ -1,8 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { BasicTablePropertyType, BasicTableRowPropertyVariants } from 'src/app/enums/basic-enum';
-import { DataCellType } from 'src/app/enums/data-cell-enum';
-import { HeaderCellType } from 'src/app/enums/header-cell-enum';
-import { NarrowBasicTableTabType } from 'src/app/enums/narrow-basic-table-tab-enum';
+import { DataCellType ,HeaderCellType ,AutoClusterTabType } from 'src/app/enums/basic-enum';
+
 
 @Component({
   selector: 'yv-cluster-narrow-basic-table-warp',
@@ -14,12 +13,38 @@ import { NarrowBasicTableTabType } from 'src/app/enums/narrow-basic-table-tab-en
 export class NarrowBasicTableWarpComponent {
 
     @Input() title: string  = '';
-    @Input() text: string  = '';
-    @Input() data: { tab : NarrowBasicTableTabType ; Headers: { data: string; type: HeaderCellType }[] ; Rows: { property: BasicTableRowPropertyVariants; showAction: boolean; cells: { data: string; type: DataCellType }[] }[]} = { property: BasicTablePropertyType.NEWֹֹֹֹֹֹ_SUGGESTIONS, Headers: [], Rows: [] };
+    @Input() subTitle: string  = '';
+    @Input() data: { [tab : AutoClusterTabType] :{ Headers: { data: string; type: HeaderCellType }[] , Rows: { property: BasicTableRowPropertyVariants; showAction: boolean; cells: { data: string; type: DataCellType }[] }[]}} =
+    {[AutoClusterTabType.SAPIR_CLUSTERS]: {
+      Headers: [
+        { data: 'initialization value', type: HeaderCellType.TEXT }
+       
+      ],
+      Rows: [
+        { 
+          property: BasicTableRowPropertyVariants.DEFAULT, 
+          showAction: true, 
+          cells: [
+            { data: 'initialization value', type: DataCellType.TEXT }
+          
+          ]
+        }
+      ]
+    }};
+
    
-    NarrowBasicTableTabType =NarrowBasicTableTabType;
+    currenTab : AutoClusterTabType = AutoClusterTabType.SAPIR_CLUSTERS;
+    tabStatus: string = 'active';
+  tabText: string = 'Click to change status';
+
+  onTabChange(isActive: boolean) {
+    this.tabStatus = isActive ? 'active' : 'not-active';
+    this.
+  }
+    AutoClusterTabType=AutoClusterTabType;
     HeaderCellType=HeaderCellType;
     DataCellType=DataCellType;
+    BasicTablePropertyType=BasicTablePropertyType;
     BasicTableRowPropertyVariants=BasicTableRowPropertyVariants;
 
 }
--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -13,11 +13,12 @@ import { AssigneeComponent } from '../basic-components/assignee/assignee.compone
 import { CheckType, CheckStateType } from 'src/app/enums/basic-enum';
 import { CheckComponent } from '../basic-components/check/check.component';
 import { SliderComponent } from '../basic-components/slider/slider.component';
+import { BasicTabComponent } from '../basic-components/basic-tab/basic-tab.component';
 
 @Component({
   selector: 'yv-cluster-test',
   standalone: true,
-  imports: [AssigneeComponent, CommonModule,CheckComponent , SliderComponent ],
+  imports: [AssigneeComponent, CommonModule,CheckComponent , SliderComponent , BasicTabComponent ],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
@@ -25,4 +26,14 @@ export class TestComponent {
   CheckType = CheckType;
   CheckStateType = CheckStateType;
   DataCellType=DataCellType;
+
+
+
+  tabStatus: string = 'active';
+  tabText: string = 'Click to change status';
+
+  onTabChange(isActive: boolean) {
+    this.tabStatus = isActive ? 'active' : 'not-active';
+    console.log(`Tab status changed to: ${this.tabStatus}`);
+  }
 }
