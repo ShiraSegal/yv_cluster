3c27fb10a0bcaa68a8d7f34b81246fd6cf802f78
אלישבע קצבורג <215554320@mbydomain>
2025-03-20 14:30:45 +0200

    field

--- src/app/components/basic-components/viewer/viewer.component.ts
+++ src/app/components/basic-components/viewer/viewer.component.ts
@@ -1,4 +1,36 @@
-import { Component, Input } from '@angular/core';
+// import { Component, Input } from '@angular/core';
+// import { ViewerIconType } from 'src/app/enums/icon-enum';
+// import { CommonModule } from '@angular/common';
+// import { IconButtonLargeType } from 'src/app/enums/basic-enum';
+// import { IconButtonLargeComponent } from '../icon-button-large/icon-button-large.component';
+
+// @Component({
+//   selector: 'yv-cluster-viewer',
+//   standalone: true,
+//   imports: [IconButtonLargeComponent,CommonModule],
+//   templateUrl: './viewer.component.html',
+//   styleUrl: './viewer.component.scss'
+// })
+// export class ViewerComponent {
+// @Input() pictureName: string = '';
+// viewerIconType = ViewerIconType;
+// iconButtonLargeType = IconButtonLargeType;
+// rotateLeft(){
+
+// }
+// rotateRight(){
+
+// }
+// print(){
+// }
+// zoomOut(){
+
+// }
+// zoomIn(){
+
+// }
+// }
+import { Component, Input, ElementRef, ViewChild, Renderer2 } from '@angular/core';
 import { ViewerIconType } from 'src/app/enums/icon-enum';
 import { CommonModule } from '@angular/common';
 import { IconButtonLargeType } from 'src/app/enums/basic-enum';
@@ -7,26 +39,44 @@ import { IconButtonLargeComponent } from '../icon-button-large/icon-button-large
 @Component({
   selector: 'yv-cluster-viewer',
   standalone: true,
-  imports: [IconButtonLargeComponent,CommonModule],
+  imports: [IconButtonLargeComponent, CommonModule],
   templateUrl: './viewer.component.html',
-  styleUrl: './viewer.component.scss'
+  styleUrls: ['./viewer.component.scss']
 })
 export class ViewerComponent {
-@Input() pictureName: string = '';
-viewerIconType = ViewerIconType;
-iconButtonLargeType = IconButtonLargeType;
-rotateLeft(){
+  @Input() pictureName: string = '';
+  viewerIconType = ViewerIconType;
+  iconButtonLargeType = IconButtonLargeType;
+  isZoomedIn: boolean = false;
+  rotation: number = 0; // initial rotation in degrees
 
-}
-rotateRight(){
+  @ViewChild('imageElement', { static: true }) imageElement!: ElementRef;
 
-}
-print(){
-}
-zoomOut(){
+  constructor(private renderer: Renderer2) {}
 
-}
-zoomIn(){
+  zoomIn() {
+    this.isZoomedIn = true;
+  }
 
-}
+  zoomOut() {
+    this.isZoomedIn = false;
+  }
+
+  rotateLeft() {
+    this.rotation -= 90;
+    this.updateTransform();
+  }
+
+  rotateRight() {
+    this.rotation += 90;
+    this.updateTransform();
+  }
+
+  print() {
+    // Implement print logic here
+  }
+
+  updateTransform() {
+    this.renderer.setStyle(this.imageElement.nativeElement, 'transform', `rotate(${this.rotation}deg)`);
+  }
 }
