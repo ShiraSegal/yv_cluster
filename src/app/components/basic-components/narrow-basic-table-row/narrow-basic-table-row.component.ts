188a6a173c6ea99db321e26e0bc716eef7b537d7
בתיה הלפרין <215553710@mbydomain>
2025-03-23 14:12:28 +0200

    update narrow

--- src/app/components/basic-components/narrow-basic-table-row/narrow-basic-table-row.component.ts
+++ src/app/components/basic-components/narrow-basic-table-row/narrow-basic-table-row.component.ts
@@ -1,25 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { DataCellType, NarrowBasicTableRowInputState } from 'src/app/enums/basic-enum';
-
+ 
 import { DataCellsComponent } from '../data-cells/data-cells.component';
-
+ 
 @Component({
   selector: 'yv-cluster-narrow-basic-table-row',
   standalone: true,
-  imports: [CommonModule,DataCellsComponent],
+  imports: [CommonModule ,DataCellsComponent],
   templateUrl: './narrow-basic-table-row.component.html',
   styleUrl: './narrow-basic-table-row.component.scss'
 })
 export class NarrowBasicTableRowComponent {
-  @Input() property: NarrowBasicTableRowInputState  = NarrowBasicTableRowInputState.DEFAULT ; 
-  @Input() cells: { data: string; type: DataCellType }[] = []; 
+  @Input() property: NarrowBasicTableRowInputState  = NarrowBasicTableRowInputState.DEFAULT ;
+  @Input() cells: { data: string; type: DataCellType }[] = [];
   DataCellType = DataCellType;
-
-  // האם צריך להוריד? ישמש רק במקרה שירצו לשנות את המצב אחרי הקריאה הראשונית האם יש מקרה כזה??
-  // changeState(newState: NarrowBasicTableRowInputState) {
-  //   this.property = newState;
-  // }
-
- 
 }
