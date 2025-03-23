c9cb88e806e74854bac93b8c3646144062530642
unknown <38327796876@mby.co.il>
2025-03-24 00:47:02 +0200

    working on viewer button

--- src/app/components/basic-components/viewer/viewer.component.ts
+++ src/app/components/basic-components/viewer/viewer.component.ts
@@ -3,55 +3,65 @@ import { CommonModule } from '@angular/common';
 import { IconButtonLargeType } from 'src/app/enums/basic-enum';
 import { IconButtonLargeComponent } from '../icon-button-large/icon-button-large.component';
 import { IconType } from 'src/app/enums/icon-enum';
-import { IconButtonComponent } from '../icon-button/icon-button.component';
 
 @Component({
   selector: 'yv-cluster-viewer',
   standalone: true,
-  imports: [IconButtonLargeComponent,CommonModule],
+  imports: [IconButtonLargeComponent, CommonModule],
   templateUrl: './viewer.component.html',
-  styleUrl: './viewer.component.scss'
+  styleUrls: ['./viewer.component.scss']
 })
 export class ViewerComponent {
-@Input() pictureName: string = '';
-iconType = IconType;
-iconButtonLargeType = IconButtonLargeType;
-rotations = [
-  'rot-0', 'rot-90', 'rot-180', 'rot-270', 
-  'rot-360', 'rot-minus-90', 'rot-minus-180', 'rot-minus-270'
-];
-scale: number = 1;  // ערך ברירת המחדל (100% זום)
+  @Input() pictureName: string = '';
+  iconType = IconType;
+  iconButtonLargeType = IconButtonLargeType;
+  rotations = [
+    'rot-0', 'rot-90', 'rot-180', 'rot-270', 'rot-360',
+  ];
+ 
+  scales = [
+    'scale-1', 'scale-1-1', 'scale-1-2', 'scale-1-3', 'scale-1-4', 'scale-1-5'
+  ];
+  scale: number = 1; 
   rotate: number = 0;
-  index:number = 0;
+  index: number = 0;
+  scaleIndex: number = 0;
   rotationClass = 'rot-0';
-  // פונקציה להגדלת התמונה
-  zoomIn() {
-    this.scale += 0.1;  // הגדל את הערך ב-10% בכל לחיצה
+  zoomClass = 'scale-1';
+
+  zoomIn() {  
+    if (this.scaleIndex < this.scales.length - 1) {
+      this.scaleIndex++;
+      this.zoomClass = this.scales[this.scaleIndex];
+      console.log('zoomIn:', this.zoomClass); 
+    }
   }
- 
-  // פונקציה להקטנת התמונה
+
   zoomOut() {
-    this.scale = Math.max(0.1, this.scale - 0.1);  // הקטנה ב-10%, לא פחות מ-10% זום
+    if (this.scaleIndex > 0) {
+      this.scaleIndex--;
+      this.zoomClass = this.scales[this.scaleIndex];
+      console.log('zoomOut:', this.zoomClass); 
+    }
   }
- //  let newRotation = (this.rotationIndex + direction / 90) % this.rotations.length;
- 
+
   rotateLeft() {
+    console.log('rotateLeft called');
     this.index = (this.index - 1 + this.rotations.length) % this.rotations.length;
     this.rotate -= 90;
     this.rotationClass = this.rotations[this.index];
+    console.log('rotateLeft:', this.rotationClass);
   }
  
   rotateRight() {
-    this.index = (this.index + 1 + this.rotations.length) % this.rotations.length;
+    console.log('rotateRight called');
+    this.index = (this.index + 1) % this.rotations.length;
     this.rotate += 90;
     this.rotationClass = this.rotations[this.index];
-  }
- 
-  print() {
-   
+    console.log('rotateRight:', this.rotationClass);
   }
 
-  // get rotationClass() {
-  //   return 'rot-90';
-  // }
+  print() {
+    // Implement print functionality if needed
+  }
 }
