7ab51d307f2718c24bb8faaaf276379551b357de
38214938334@mby.co.il <38214938334@mby.co.il>
2025-05-06 20:53:11 +0300

    filter section after changes to ControlValueAccessor

--- src/app/components/basic-components/filter-section/filter-section.component.ts
+++ src/app/components/basic-components/filter-section/filter-section.component.ts
@@ -1,8 +1,19 @@
+
 import { Component, Input } from '@angular/core';
-import { ButtonSize, ButtonType, IconButtonLargeType, NativeOptionState, NativeOptionType, State } from 'src/app/enums/basic-enum';
-import { ButtonComponent } from '../button/button.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
+import {
+  ButtonSize,
+  ButtonType,
+  IconButtonLargeType,
+  NativeOptionState,
+  NativeOptionType,
+  State
+} from 'src/app/enums/basic-enum';
 import { IconType } from 'src/app/enums/icon-enum';
+
+import { ButtonComponent } from '../button/button.component';
 import { IconButtonLargeComponent } from '../icon-button-large/icon-button-large.component';
 import { SelectComponent } from '../select/select.component';
 import { FieldComponent } from '../field/field.component';
@@ -10,32 +21,60 @@ import { FieldComponent } from '../field/field.component';
 @Component({
   selector: 'yv-cluster-filter-section',
   standalone: true,
-  imports: [CommonModule,ButtonComponent,IconButtonLargeComponent,SelectComponent,FieldComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ButtonComponent,
+    IconButtonLargeComponent,
+    SelectComponent,
+    FieldComponent
+  ],
   templateUrl: './filter-section.component.html',
-  styleUrl: './filter-section.component.scss'
+  styleUrls: ['./filter-section.component.scss']
 })
 export class FilterSectionComponent {
+  @Input() buttonText: string = 'New Cluster';
+  @Input() icon: IconType = IconType.PLUS_LIGHT;
 
-  @Input() buttonText: string = 'test';
-  @Input() icon:IconType = IconType.PLUS_REGULAR;
- stateEnum = State;
-  ButtonSize = ButtonSize;
-  primary = ButtonType.PRIMARY
- nativeOptions = NativeOptionType;
+  filterForm: FormGroup;
 
-  iconType = IconType;//  types of icons.
-    iconButtonLargeType = IconButtonLargeType;
+  // enums
+  stateEnum = State;
+  nativeOptions = NativeOptionType;
+  iconType = IconType;
+  iconButtonLargeType = IconButtonLargeType;
+  primary = ButtonType.PRIMARY;
+  ButtonType = ButtonType;
+
   assigneeOptions = [
     { optionType: NativeOptionType.ASSIGNEE, optionState: NativeOptionState.DEFAULT },
     { optionType: NativeOptionType.ASSIGNEE, optionState: NativeOptionState.DEFAULT },
     { optionType: NativeOptionType.ASSIGNEE, optionState: NativeOptionState.DEFAULT }
   ];
+
   badgeOptions = [
     { optionType: NativeOptionType.STATUS, optionState: NativeOptionState.DEFAULT },
-    { optionType: NativeOptionType.STATUS, optionState: NativeOptionState.DEFAULT },
+    { optionType: NativeOptionType.STATUS, optionState: NativeOptionState.DEFAULT }
   ];
-  onClick() {
-    alert('test on click');
-    console.log('test on click');
+
+  constructor(private fb: FormBuilder) {
+    this.filterForm = this.fb.group({
+      search: [''],
+      date: [null],
+      status: [null],
+      assignee: [null],
+      toggleAssignee: [false],
+      toggleStatus: [false]
+    });
+
+    this.filterForm.valueChanges.subscribe(val => {
+      console.log('form value:', val);
+    });
   }
+
+  onClick() {
+    console.log('Submit clicked:', this.filterForm.value);
+  }
+
+
 }
