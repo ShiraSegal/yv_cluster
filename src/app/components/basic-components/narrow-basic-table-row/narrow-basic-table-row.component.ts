e48fa54ff2128a079e96eeab6a7d6485b917ca4c
ליף אפרת <efrat.liff@yadvashem.org.il>
2025-06-18 15:14:26 +0300

    update form by efrat

--- src/app/components/basic-components/narrow-basic-table-row/narrow-basic-table-row.component.ts
+++ src/app/components/basic-components/narrow-basic-table-row/narrow-basic-table-row.component.ts
@@ -18,7 +18,7 @@ export class NarrowBasicTableRowComponent {
   @Input() property: NarrowBasicTableRowInputState = NarrowBasicTableRowInputState.DEFAULT;
   @Input() length: NarrowBasicTableRowLength;
   @Input() currentTab: AutoClusterTabType;
-  @Input() rowGroup : FormGroup<any>;
+  @Input() rowGroup : FormGroup;
   @Input() prefCodeStatus: boolean = false;
   @Output() bookIdToDelet = new EventEmitter<string>();
   subscription: Subscription = new Subscription();
@@ -38,19 +38,40 @@ export class NarrowBasicTableRowComponent {
   iconType = IconType;
   buttonType = ButtonType;
   rowGroupControls: { control: FormControl, name: string }[];
- 
+
   ngOnInit() {
-    this.rowGroupControls = Object.keys(this.rowGroup.controls).map(controlKey => {
+    this.updateControlsArray()
+    this.subscription.add(this.rowGroup.valueChanges.subscribe((value) => {
+      console.log('RowGroup changed: 💜', value);
+          this.updateControlsArray()
+        console.log("❄️",this.rowGroupControls );
+
+    }))
+  }
+  updateControlsArray(){
+ this.rowGroupControls = Object.keys(this.rowGroup.controls).map(controlKey => {
       return {
         control: this.rowGroup.controls[controlKey] as FormControl, // קונטרול מסוג FormControl
         name: controlKey // שם הקונטרול
       };
     });
-    this.subscription.add(this.rowGroup.valueChanges.subscribe((value) => {
-      console.log('RowGroup changed:', value);
-    }))
   }
+ngOnChanges(changes: SimpleChanges): void {
+  if (changes['rowGroup']) {
+    this.updateControlsArray();
 
+    this.subscription.unsubscribe(); // נקה את כל המנויים הקודמים
+    this.subscription = new Subscription();
+
+    // הירשם מחדש לשינויים בטופס
+    this.subscription.add(
+      this.rowGroup.valueChanges.subscribe((value) => {
+        console.log('RowGroup changed: 💜', value);
+        this.updateControlsArray();
+      })
+    );
+  }
+}
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe()
